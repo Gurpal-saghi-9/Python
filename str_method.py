8862c4577c1ac382512a4c84ad401850bd95f817
guru <gurusaghi324@gmail.com>
2024-12-24 20:22:42 +0530

    new

---
+++ str_method.py
@@ -0,0 +1,15 @@
+str1 = "gur"
+str2 = "pal"
+
+result = str1 + str2;
+
+print(result)
+
+print(len(result))
+
+print(str1.upper())
+
+str3 = "Hello, World"
+print(str3.split(","))
+
+print("ld" in str3)
