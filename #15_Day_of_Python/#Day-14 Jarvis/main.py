4c8efb9d009dcf0e3691c7c93890c64a4798ba72
guru <gurusaghi324@gmail.com>
2025-01-09 20:46:53 +0530

    commit

--- #15_Day_of_Python/#Day-14 Jarvis/main.py
+++ #15_Day_of_Python/#Day-14 Jarvis/main.py
@@ -10,7 +10,7 @@ import os
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
-# newsapi = "12d21c3618c748b29d7155e35479830a"
+newsapi = "12d21c3618c748b29d7155e35479830a"
 
 # Function to set the voice to female
 def set_female_voice():
@@ -50,7 +50,8 @@ def speak(text):
 #     os.remove("temp.mp3") 
 
 def aiProcess(command):
-    # client = OpenAI(api_key="yor_api")
+    client = OpenAI(api_key="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",
+    )
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
@@ -85,7 +86,7 @@ def processCommand(c):
         #     speak("Sorry, I don't have that song in my library.")
 
     elif "news" in c.lower():
-        # r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey=12d21c3618c748b29d7155e35479830a")
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey=12d21c3618c748b29d7155e35479830a")
         if r.status_code == 200:
             #press the JSON response
             data = r.json()
