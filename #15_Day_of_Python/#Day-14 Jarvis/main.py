58a341c1dde82bfe6bf0523bfed036cf2ec540d8
guru <gurusaghi324@gmail.com>
2025-01-09 20:44:04 +0530

    commit

--- #15_Day_of_Python/#Day-14 Jarvis/main.py
+++ #15_Day_of_Python/#Day-14 Jarvis/main.py
@@ -50,7 +50,7 @@ def speak(text):
 #     os.remove("temp.mp3") 
 
 def aiProcess(command):
-    # client = OpenAI(api_key="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")
+    # client = OpenAI(api_key="yor_api")
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
