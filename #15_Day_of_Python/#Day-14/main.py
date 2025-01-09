0691c8a60122c308fe69581379a68a3a17bd81e1
guru <gurusaghi324@gmail.com>
2025-01-09 19:50:42 +0530

    commit

--- #15_Day_of_Python/#Day-14/main.py
+++ #15_Day_of_Python/#Day-14/main.py
@@ -4,14 +4,37 @@ import pyttsx3
 import musicLibrary
 import requests
 from openai import OpenAI
+from gtts import gTTS
+import pygame
+import os
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 newsapi = "12d21c3618c748b29d7155e35479830a"
 
-def speak(text):
+def speak_old(text):
     engine.say(text)
     engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+
+     # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
 
 def aiProcess(command):
     client = OpenAI(api_key="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",
