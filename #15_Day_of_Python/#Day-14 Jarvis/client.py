8639cf8dfba78c4d618e115f1322b84c9868bb87
guru <gurusaghi324@gmail.com>
2025-01-09 20:42:12 +0530

    commit

--- #15_Day_of_Python/#Day-14 Jarvis/client.py
+++ #15_Day_of_Python/#Day-14 Jarvis/client.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 # pip install openai 
 # if you saved the key under a different environment variable name, you can do something like:
 client = OpenAI(
-  api_key="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",
+  # api_key="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",
 )
 
 completion = client.chat.completions.create(
--- #15_Day_of_Python/#Day-14 Jarvis/main.py
+++ #15_Day_of_Python/#Day-14 Jarvis/main.py
@@ -10,7 +10,7 @@ import os
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
-newsapi = "12d21c3618c748b29d7155e35479830a"
+# newsapi = "12d21c3618c748b29d7155e35479830a"
 
 # Function to set the voice to female
 def set_female_voice():
@@ -50,8 +50,7 @@ def speak(text):
 #     os.remove("temp.mp3") 
 
 def aiProcess(command):
-    client = OpenAI(api_key="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",
-    )
+    # client = OpenAI(api_key="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")
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
