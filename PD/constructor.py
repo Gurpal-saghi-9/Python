659f1d41597a1234c8664231f8b7e1cffc16ac6a
guru <gurusaghi324@gmail.com>
2025-04-04 13:39:29 +0530

    commit

---
+++ PD/constructor.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+# Example usage
+person = Person("Gurpal Singh", 30)
+person.display_info()
