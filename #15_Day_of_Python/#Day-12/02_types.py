7cb9408f783d6636ae11895034d8614945f76f12
gurpal-saghi-9 <gurusaghi324@gmail.com>
2025-07-15 07:05:58 +0530

    commit

--- #15_Day_of_Python/#Day-12/02_types.py
+++ #15_Day_of_Python/#Day-12/02_types.py
@@ -7,3 +7,6 @@ name: str = "Harry"
 
 def  sum(a: int, b: int) -> int:
     return a+b
+
+sum = sum(2, 3.5)
+print(sum)
--- Bro code/pattern.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter the number of rows: "))
-
-for i in range(1, n + 1):
-    print(" " * (n - i), end="")  # Print leading spaces
-    for j in range(1, 2 * i):   # Print numbers
-        print(j, end="")
-    print()  # Move to the next line
